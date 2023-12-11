ae26962f16db3bf0743529017fb5eef76a1c58ae
vincent-kipngeno <kipngenovi2001@gmail.com>
2023-12-11 10:58:30 +0300

    Add notification service

---
+++ notification/src/main/java/org/example/notification/NotificationApplication.java
@@ -0,0 +1,38 @@
+package org.example.notification;
+
+import org.example.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.properties.PropertyMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.example.notification",
+                "org.example.amqp",
+        }
+)
+@PropertyMapping("classpath:clients-${spring.profiles.active}.properties")
+public class NotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//            ) {
+//        return args -> {
+//            producer.publish(
+//                    new Person("Ali", 18),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
+}
