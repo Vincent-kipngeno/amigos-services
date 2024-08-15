ed35cdc5c9fd9f94882f0d6002dd4e4094876c27
vincent-kipngeno <kipngenovi2001@gmail.com>
2024-08-15 13:06:39 +0300

    Add stored procedure

--- deploy.sh
+++ deploy.sh
@@ -69,3 +69,50 @@ kubectl get services
 kubectl exec -it postgres-0 -- psql -U vincent
 
 minikube service customer --url
+
+#/*
+#  CREATE DEFINER=`ewrsdevu1`@`%` PROCEDURE `GenerateCustomID`(IN tableName VARCHAR(255), IN idName VARCHAR(255), OUT custom_id VARCHAR(20))
+#  BEGIN
+#      DECLARE prefix CHAR(1);
+#      DECLARE suffix CHAR(3);
+#      DECLARE digits1 CHAR(1);
+#      DECLARE digits2 CHAR(1);
+#      DECLARE digits3 CHAR(1);
+#      DECLARE mid_letter CHAR(1);
+#      DECLARE digit_mid CHAR(1);
+#      DECLARE letter_end CHAR(1);
+#      DECLARE digit_end1 CHAR(1);
+#      DECLARE digit_end2 CHAR(1);
+#      DECLARE new_id VARCHAR(20);
+#
+#      DECLARE is_unique BOOLEAN DEFAULT FALSE;
+#
+#      WHILE NOT is_unique DO
+#          SET prefix = CHAR(FLOOR(1 + RAND() * 9) + 48); -- Random digit
+#          SET suffix = CHAR(FLOOR(1 + RAND() * 9) + 48); -- Random digit
+#          SET digits1 = CHAR(FLOOR(1 + RAND() * 9) + 48); -- Random digit
+#          SET digits2 = CHAR(FLOOR(1 + RAND() * 9) + 48); -- Random digit
+#          SET digits3 = CHAR(FLOOR(1 + RAND() * 9) + 48); -- Random digit
+#          SET mid_letter = CHAR(FLOOR(0 + RAND() * 25) + 65); -- Random letter
+#          SET digit_mid = CHAR(FLOOR(1 + RAND() * 9) + 48); -- Random digit
+#          SET letter_end = CHAR(FLOOR(0 + RAND() * 25) + 65); -- Random letter
+#          SET digit_end1 = CHAR(FLOOR(1 + RAND() * 9) + 48); -- Random digit
+#          SET digit_end2 = CHAR(FLOOR(1 + RAND() * 9) + 48); -- Random digit
+#
+#          SET new_id = CONCAT(prefix, mid_letter, digits1, digits2, digits3, mid_letter, digit_mid, letter_end, digit_end1, digit_end2);
+#
+#          -- Check for uniqueness
+#          SET @sql = CONCAT('SELECT 1 FROM ', tableName, ' WHERE ', idName, ' = ?');
+#          PREPARE stmt FROM @sql;
+#          SET @param1 = new_id;
+#          EXECUTE stmt USING @param1;
+#          DEALLOCATE PREPARE stmt;
+#
+#          IF NOT FOUND_ROWS() THEN
+#              SET is_unique = TRUE;
+#          END IF;
+#      END WHILE;
+#
+#      SET custom_id = new_id;
+#  END
+# */
